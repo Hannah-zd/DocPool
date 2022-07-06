309a41a1237e42dacb6bad3ffc9abf466d47a760
zhangd <1369398555@qq.com>
2022-07-06 14:16:13 +0800

    recompile

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_book_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -47,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'press'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [sphinx_book_theme.get_html_theme_path()]
+html_static_path = ['_static']
